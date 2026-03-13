2ac46bc8cb07423d0ef34d1602d487e98590628d
Jitesh Jain <jiteshjain2004@gmail.com>
2026-03-13 11:39:07 +0530

    Made API endpoints clickable

--- EXP6/exp6.2/app.js
+++ EXP6/exp6.2/app.js
@@ -25,14 +25,22 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/",(req,res)=>{
     res.send(`
     <h1>JWT Banking API</h1>
+
     <h3>Endpoints</h3>
+
     <ul>
-        <li>POST /api/register</li>
-        <li>POST /api/login</li>
-        <li>POST /api/refresh-token</li>
-        <li>GET /api/bank/account</li>
-        <li>GET /api/bank/balance</li>
+        <li><a href="/api/register">POST /api/register</a></li>
+        <li><a href="/api/login">POST /api/login</a></li>
+        <li><a href="/api/refresh-token">POST /api/refresh-token</a></li>
+        <li><a href="/api/bank/account">GET /api/bank/account</a></li>
+        <li><a href="/api/bank/balance">GET /api/bank/balance</a></li>
     </ul>
+
+    <p><b>Note:</b></p>
+    <p>POST routes must be tested using Postman.</p>
+    <p>Protected routes require header:</p>
+
+    <pre>Authorization: Bearer YOUR_TOKEN</pre>
     `)
 })
 
