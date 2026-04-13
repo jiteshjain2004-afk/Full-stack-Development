4dcf5ee0402caaf9c07b57c8dce361179ac1c8bb
Jitesh Jain <jiteshjain2004@gmail.com>
2026-04-13 18:17:50 +0530

    Remove exp10 folders from repository

--- EXP10/exp10.3/social-app/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region"   { default = "us-east-1" }
-variable "project_name" { default = "social-app" }
-variable "mongo_uri"    { description = "MongoDB Atlas URI" }
-variable "jwt_secret"   { description = "JWT secret key" }
